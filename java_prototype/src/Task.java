a1a4b743e8a4d13e719d1a309ed7de1042f179b5
JUB_Yoush <yoush1337@gmail.com>
2022-03-19 08:35:12 -0400

    java classes

---
+++ java_prototype/src/Task.java
@@ -0,0 +1,43 @@
+public class Task {
+    String name;
+    String courseName;
+    double percentage;
+    double gradeWeight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public double getGradeWeight() {
+        return gradeWeight;
+    }
+
+    public void setGradeWeight(double gradeWeight) {
+        this.gradeWeight = gradeWeight;
+    }
+
+    public void addGrade(double grade){
+
+    }
+
+}
