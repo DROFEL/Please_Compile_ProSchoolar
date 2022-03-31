e61260d35062719de862edd36552c629f4f74219
JUB_Yoush <yoush1337@gmail.com>
2022-03-31 11:22:19 -0400

    everything is inside the MainActivity.java file now

--- app/src/main/java/com/example/please_compile_proschoolar/MainActivity.java
+++ app/src/main/java/com/example/please_compile_proschoolar/MainActivity.java
@@ -2,41 +2,89 @@ package com.example.please_compile_proschoolar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 public class  MainActivity extends AppCompatActivity {
-    private Student currentStudent;
-    private Button btnMainLogin;
-    private  Button btnAddTask;
-    private TextView title;
-    private TextView test;
 
+    private Student currentStudent;
+
+    //MAIN LAYOUT
+    private Button btnMainLogin;
+    private Button btnMainAddTask;
+    private Button btnMainLeft;
+    private Button btnMainRight;
+    private TextView txvMainTitle;
+
+    // TASK INFORMATION
+    private TextView txvMainTaskName;
+    private TextView txvMainTaskCourse;
+    private TextView txvMainTaskGradePrecent;
+    private TextView txvMainTaskGradeWeight;
+    private TextView txvMainTaskDueDate;
+
+    //LOGIN LAYOUT
+    private TextView txvLoginTitle;
+    private EditText etxtLoginName;
+    private EditText etxtLoginPassword;
+    private Button btnLoginSubmit;
+    private Button btnLoginSignup;
+
+
+    //SIGNUP LAYOUT
+    private TextView txvSignupTitle;
+    private EditText etxtSignupName;
+    private EditText etxtSignupPassword;
+    private Button btnSignupRegister;
+    private Button btnSignupReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //MAIN LAYOUT
         btnMainLogin = (Button) findViewById(R.id.btnMainLogin);
-        btnAddTask = (Button) findViewById(R.id.btnMainLogin);
-        title = (TextView) findViewById(R.id.txvMainTitle);
-        test = (TextView) findViewById(R.id.txvTestText);
-        test.setText();
+        btnMainAddTask = (Button) findViewById(R.id.btnMainAddTask);
+        btnMainLeft = (Button) findViewById(R.id.btnMainLeft);
+        btnMainRight = (Button) findViewById(R.id.btnMainRight);
+        txvMainTitle = (TextView) findViewById(R.id.txvMainTitle);
+
+        //TASK LAYOUT
+        txvMainTaskName =(TextView) findViewById(R.id.txvMainTaskName);
+        txvMainTaskCourse =(TextView) findViewById(R.id.txvMainTaskCourse);
+        txvMainTaskGradePrecent =(TextView) findViewById(R.id.txvMainTaskGradePercent);
+        txvMainTaskGradeWeight =(TextView) findViewById(R.id.txvMainTaskGradeWeight);
+        txvMainTaskDueDate =(TextView) findViewById(R.id.txvMainTaskDueDate);
+
+        //LOGIN LAYOUT
+        txvLoginTitle = (TextView) findViewById(R.id.txvLoginTitle);
+        etxtLoginName = (EditText) findViewById(R.id.etxtLoginName);
+        etxtLoginPassword = (EditText) findViewById(R.id.etxtLoginPassword);
+        btnLoginSignup = (Button) findViewById(R.id.btnLoginSignup);
+        btnLoginSubmit = (Button) findViewById(R.id.btnLoginSubmit);
+
+        //SIGNUP LAYOUT
+        txvSignupTitle = (TextView) findViewById(R.id.txvSignupTitle);
+        etxtSignupName = (EditText) findViewById(R.id.etxtSignupName);
+        etxtSignupPassword = (EditText) findViewById(R.id.etxtSignupPassword);
+        btnSignupRegister = (Button) findViewById(R.id.btnSignupRegister);
+        btnSignupReturn = (Button) findViewById(R.id.btnSignupReturn);
+
+
+
     }
     public void gotoLoginScreen(View view) {
         //Intent intent = new Intent(this, LoginActivity.class);
         //startActivity(intent);
-        setContentView(R.layout.test_layout);
-        DataSingle.getInstance()
+        setContentView(R.layout.login_layout);
+       // DataSingle.getInstance();
     }
     public void addTaskClicked(View view){
-        title.setText("Add task");
+        txvMainTitle.setText("Add task");
     }
 
 
