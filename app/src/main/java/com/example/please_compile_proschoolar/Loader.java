5a8caa4eebed55a63d36798a082927e42c2b0cf9
George Retinskiy <cradplay@gmail.com>
2022-04-02 16:17:10 -0400

    gson added

--- app/src/main/java/com/example/please_compile_proschoolar/Loader.java
+++ app/src/main/java/com/example/please_compile_proschoolar/Loader.java
@@ -1,12 +1,24 @@
 package com.example.please_compile_proschoolar;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 public class Loader {
 
     public static void addStudent(Student student){
 
+        ArrayList<Student> studentArray = new ArrayList<>();
+
+        Gson gson = new Gson();
+
+        String JSON ;
+
     }
 
     public static Student loadStudent(String name){
+
+
         return new Student(name, name);
     }
 }
